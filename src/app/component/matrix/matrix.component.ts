046145aeda35312b79a50222ab505dc306d3458a
DESKTOP-EFBU197\User <yuyueh.chang@gmail.com>
2022-05-23 20:34:23 +0800

    resize layout and remove unused html

--- src/app/component/matrix/matrix.component.ts
+++ src/app/component/matrix/matrix.component.ts
@@ -1,9 +1,7 @@
-import { Tile } from './../../core/model/tile';
 import { AppState } from './../../store/app.state';
 import { TetrisSelectors } from './../../store/tetris/tetris.selector';
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { select, Store } from '@ngrx/store';
-import { TetrisActions } from 'src/app/store/tetris/tetris.actions';
 import { KeyboardSelectors } from 'src/app/store/keyboard/keyboard.selector';
 
 @Component({
